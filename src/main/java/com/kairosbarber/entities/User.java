222afa0995ee6175c2ef14e14db444a98c755829
Salome Ramirez <salomeramirezsanchez2004@gmail.com>
2025-03-05 01:31:34 -0500

    Create User.java

---
+++ src/main/java/com/kairosbarber/entities/User.java
@@ -0,0 +1,24 @@
+package com.kairosbarber.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
